f5b97e488bdd4ffda18027befccd003b38fc8642
carlaluquearrufat-lab <carla.luque.arrufat@estudiantat.upc.edu>
2025-10-17 18:14:11 +0200

    Update and rename recibir_info.py (test unitario) to recibir_info.py

--- recibir_info.py
+++ recibir_info.py
@@ -1,3 +1,4 @@
+#Test unitario
 import serial
 device = 'COM3'
 mySerial = serial.Serial(device, 9600)
@@ -5,3 +6,4 @@ while True:
    if mySerial.in_waiting > 0:
       linea = mySerial.readline().decode('utf-8').rstrip()
       print(linea)
+
