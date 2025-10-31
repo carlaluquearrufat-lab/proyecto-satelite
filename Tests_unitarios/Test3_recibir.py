a504ec0f583933ecd23bbdb472404096ef592b9a
arnaupratvillarroya <arnau.prat.villarroya@estudiantat.upc.edu>
2025-10-31 13:21:35 +0100

    Create Test3_recibir.py

---
+++ Tests_unitarios/Test3_recibir.py
@@ -0,0 +1,8 @@
+#Test unitario
+import serial
+device = 'COM3'
+mySerial = serial.Serial(device, 9600)
+while True:
+   if mySerial.in_waiting > 0:
+      linea = mySerial.readline().decode('utf-8').rstrip()
+      print(linea)
