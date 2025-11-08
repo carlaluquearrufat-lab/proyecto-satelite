aab83290da56e50d4453ef43e688de8f903058d6
carlaluquearrufat-lab <carla.luque.arrufat@estudiantat.upc.edu>
2025-11-08 18:41:29 +0100

    Update Satelite.ino

--- Satelite.ino
+++ Satelite.ino
@@ -1,5 +1,6 @@
 #include <SoftwareSerial.h>
 int i;
+int contador=0;
 #include <DHT.h>
 #define DHTPIN 2
 #define DHTTYPE DHT11
@@ -22,20 +23,33 @@ void loop() {
    i=i+1;
    float h = dht.readHumidity();
    float t = dht.readTemperature();
-   digitalWrite(4, HIGH);
-   delay(500);
-   digitalWrite (4, LOW);
+  
+   while ( !isnan(h) && !isnan(t)){
+      digitalWrite(4, HIGH);
+      delay(500);
+      digitalWrite (4, LOW);
+      }
    
    if (isnan(h) || isnan(t)){
+      digitalWrite(4, LOW);
       mySerial.println("Error al leer el sensor DHT11");
+      contador++;
+      while (contador > 0){
       digitalWrite(6, HIGH);
       delay(500);
       digitalWrite (6, LOW);
+      }
+      //Si se capturan mal los datos más de tres veces enviamos mensaje para saltar alarma en tierra
+      if (contador >= 3){
+         Serial.print ("Fallo");
+      }
    }
    else {
+      contador= 0;
       mySerial.print("H: ");
       mySerial.print(h);
       mySerial.print("T: ");
       mySerial.println(t);
    }
 }
+
