13d2989614b08e3b9fc69fa65b9ef33d223f28e0
carlaluquearrufat-lab <carla.luque.arrufat@estudiantat.upc.edu>
2025-11-08 17:50:43 +0100

    Update Tierra.ino

--- Tierra.ino
+++ Tierra.ino
@@ -1,12 +1,14 @@
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(10, 11); // RX, TX (azul, naranja)
 unsigned long nextMillis = 500;
+const int pinAlarma = 8;
 
 void setup() {
    Serial.begin(9600);
    Serial.println("Empezamos la recepción");
    mySerial.begin(9600);
    pinMode(5, OUTPUT);
+   pinMode(pinAlarma, OUTPUT);
 }
 void loop() {
    if (mySerial.available()) {
@@ -17,4 +19,8 @@ void loop() {
          digitalWrite (5, LOW);
          delay(500);
    }
+   else {
+      digitalWrite(pinAlarma, HIGH);
+   }
 }
+
