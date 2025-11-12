4f0bc2fff0136b169551ae9e14f9a1c3f38f5b1f
carlaluquearrufat-lab <carla.luque.arrufat@estudiantat.upc.edu>
2025-11-12 11:13:17 +0100

    Update Test3_alarma.ino

--- Tests_unitarios/Test3_alarma.ino
+++ Tests_unitarios/Test3_alarma.ino
@@ -1,12 +1,12 @@
-const int pinAlarma = 8;
+int pinAlarma = 8;
 
 void setup() {
   pinMode (pinAlarma, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(pinAlarma, HIGH);
-  delay(2000);
-  digitalWrite(pinAlarma, LOW);
-  delay(2000);
+  tone(pinAlarma, 1000);
+  delay(1000);
+  noTone(pinAlarma);
+  delay(1000);
 }
